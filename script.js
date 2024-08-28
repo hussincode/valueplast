1112f06db93ed3268b9f28352f396f537fe0db9d
hussincode <hussinhesham2009@gmail.com>
2024-08-28 11:14:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const section = window.location.pathname;
+const navlink = document.querySelectorAll('header nav a');
+
+
+forEach( link =>{
+
+   if(link.href.includes(`$(section)`)){  links.classlist.add( 'active' );}
+  
+    
+})
